20e679d2d7e335de40bb5126869f182765e3b9d1
Sara Jimenez <sjimenez11@educa.madrid.org>
2024-06-06 01:15:47 +0200

    Primer commit front-end. Listado de libros

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import FooterComponent from './components/FooterComponent';
+import HeaderComponent from './components/HeaderComponent';
+import ListBooksComponent from './components/ListBooksComponent';
+
+function App() {
+  return (
+    <div>
+      <HeaderComponent/>
+      <ListBooksComponent/>
+      <FooterComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ListBooksComponent.js
@@ -0,0 +1,43 @@
+import {useState, useEffect} from 'react';
+import BookService from '../services/BookService';
+
+export const ListBooksComponent = () => {
+    
+    const [books, setBooks] = useState([]);
+
+    useEffect(() =>{
+        //llamamos al método getAllBooks para que después los datos de la respuesta se metan en setBooks
+        BookService.getAllBooks().then(response =>{
+            setBooks(response.data);
+            console.log(response.data);
+        }).catch(error =>{
+            console.log(error);
+        })
+    }, [])
+
+  return (
+    <div className='container'>
+        <h2 className='text-center'>Books</h2>
+        <table className='table table-bordered table-striped'>
+            <thead>
+                <th>Id</th>
+                <th>Título</th>
+                <th>Autor</th>
+            </thead>
+            <tbody>
+                {
+                    books.map(
+                        libro =>
+                            <tr key={libro.id}>
+                               <td>{libro.title}</td> 
+                               <td>{libro.author}</td> 
+                            </tr>
+                    )
+                }
+            </tbody>
+        </table>
+    </div>
+  )
+}
+
+export default ListBooksComponent;
