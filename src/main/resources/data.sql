408097f64f70ee0d968de61e23b84ae1e2d50901
Sara Jimenez <sjimenez11@educa.madrid.org>
2024-06-17 18:20:49 +0200

    Inserción de datos.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,6 @@
 INSERT INTO books (title, author, synopsis) VALUES ('Title1', 'Author1', 'Synopsis1');
 INSERT INTO books (title, author, synopsis) VALUES ('Title2', 'Author2', 'Synopsis1');
+
+INSERT INTO reading_Lists (user_Id, name, description) VALUES (1, 'Summer Reads', 'Books to read during summer vacation');
+
+INSERT INTO reading_List_Books (id_Book, id_Reading_List, status) VALUES (1, 1, 'LEYENDO');
